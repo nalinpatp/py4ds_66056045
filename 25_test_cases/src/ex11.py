c882ab54608040d20f44ab212a8cab358f78afda
nalinpatp <66056045@kmitl.ac.th>
2023-10-14 00:03:54 +0700

    fix function get_hr_min_sec

--- 25_test_cases/src/ex11.py
+++ 25_test_cases/src/ex11.py
@@ -3,7 +3,7 @@ Exercise 11
 """
 
 
-def get_hr_min_sec(param):
+def get_hr_min_sec(tsec):
     """
     Generates a string representation of the given number
     of seconds in the format "9h 9m 9s".
@@ -22,4 +22,31 @@ def get_hr_min_sec(param):
         >>> get_hr_min_sec(0)
         '0s'
     """
-    pass
+    temp=tsec
+    hr=0
+    min=0
+    sec=0
+    word=''
+    if tsec==0:
+        return '0s'
+    while(temp>=3600):
+        temp=temp-3600
+        hr=hr+1
+    while(temp>=60):
+        temp=temp-60
+        min=min+1
+    sec=temp
+    if hr!=0:
+        word=word+str(hr)+'h'
+    if min!=0:
+        if word!='':
+            word=word+' '+str(min)+'m'
+        else:
+            word=word+str(min)+'m'
+    if sec!=0:
+        if word!='':
+            word=word+' '+str(sec)+'s'
+        else:
+            word=word+str(sec)+'s'
+    return word
+
