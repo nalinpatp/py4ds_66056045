37903ff18070bc7e4d30cba4963c261c11a6cdfb
nalinpatp <66056045@kmitl.ac.th>
2023-10-13 23:23:13 +0700

    fix print_ASCII_table function

--- 25_test_cases/src/ex07.py
+++ 25_test_cases/src/ex07.py
@@ -3,7 +3,7 @@ Execise 7
 """
 
 
-def print_ASCII_table():
+def print_ASCII_table(start_char,end_char):
     """
     Generate and print the ASCII table for a range of characters.
 
@@ -15,5 +15,9 @@ def print_ASCII_table():
         str: Returns nothing. The function simply prints the ASCII characters.
 
     """
-    # TODO : complete this
-    pass
+    # Fix : complete this
+    if int(start_char) <= int(end_char):
+        for x in range(start_char,end_char+1):
+            print(chr(x))
+    else:
+        print('Wrong input')
