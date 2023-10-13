d456c5c49d0cca0a450aa32256b518891e50e44f
nalinpatp <66056045@kmitl.ac.th>
2023-10-14 00:57:23 +0700

    fix get_cost_of_coffee

--- 25_test_cases/src/ex18.py
+++ 25_test_cases/src/ex18.py
@@ -3,5 +3,15 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
-    pass
+def get_cost_of_coffee(cups, price):
+    free=0
+    temp=cups
+    if cups<=8:
+        return cups*price
+    else:
+        while(temp>8):
+            temp=temp-8
+            free=free+1
+        return (cups-free)*price
+
+
