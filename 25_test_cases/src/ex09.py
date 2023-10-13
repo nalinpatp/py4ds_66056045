7049df9e6167e2d2b9c46696bfe77e8416564c01
nalinpatp <66056045@kmitl.ac.th>
2023-10-13 23:33:32 +0700

    fix function get_chess_square_color

--- 25_test_cases/src/ex09.py
+++ 25_test_cases/src/ex09.py
@@ -18,5 +18,10 @@ def get_chess_square_color(col, row):
     Returns:
         str: The color of the chess square, either 'white' or 'black'.
     """
-    # TODO : complete this
-    pass
+    # Fix : complete this
+    if (col+row)%2==0:
+        return 'White'
+    elif (col+row)%2==1:
+        return 'Black'
+    else:
+        print('Wrong input')
